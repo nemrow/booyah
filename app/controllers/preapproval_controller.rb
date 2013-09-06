bb4bed96a3d2520ff43741ca4c6a14bb1100e19e
Jordan Nemrow <nemrowj@gmail.com>
2013-09-06 11:55:27 -0700

    changed confirmation text on the controllers

--- app/controllers/preapproval_controller.rb
+++ app/controllers/preapproval_controller.rb
@@ -1,6 +1,8 @@
 class PreapprovalController < ApplicationController
   def show
+    user = User.find(params[:user_id])
     preapproval = PaypalPreapproval.find(params[:id])
     preapproval.activate
+    send_welcome_message(user) if user.addresses.count == 1
   end
 end
