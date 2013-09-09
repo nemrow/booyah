7642f6a8203eb107b2ebc10332afd8c232c8df45
Jordan Nemrow <nemrowj@gmail.com>
2013-09-09 10:31:23 -0700

    paypal configs set for multi environment

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,9 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
+PayPal::SDK.configure(
+  :mode      => Rails.env.production?  ? "live" : "sandbox",
+  :app_id    => ENV['PAYPAL_APP_ID'],
+  :username  => ENV['PAYPAL_USERNAME'],
+  :password  => ENV['PAYPAL_PASSWORD'],
+  :signature => ENV['PAYPAL_SIGNATURE']
+)
+
 PayPal::SDK.logger = Rails.logger
