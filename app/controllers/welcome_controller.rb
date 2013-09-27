668cac18f0b38a951d2eb2d51a007e1d1efe7532
Jordan Nemrow <nemrowj@gmail.com>
2013-09-27 11:04:24 -0700

    setup email helpers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,15 @@ class WelcomeController < ApplicationController
   end
 
   def contact
+    p send_basic_email(
+      'nemrowj@gmail.com',
+      'this is a subjeezy',
+      'Jordannn',
+      [
+        "p num 1",
+        "p num 2",
+        "p num 3"
+      ]
+    )
   end
 end
--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -7,12 +7,35 @@ module EmailHelper
       message
       )
   end
+
+  def send_basic_email(email, subject, name, paragraphs)
+    send_email( email, subject, basic_template({ :name => name, :paragraphs => paragraphs }))
+  end
   
-  def send_email(to, subject, message, from = 'Jordan@pigeonpic.com')
-    RestClient.post "https://api:#{ENV['MAILGUN_KEY']}@api.mailgun.net/v2/booyahbooyah.com"+"/messages", 
-        :from => from,
-        :to => to,
-        :subject => subject,
-        :html => "<b>HTML</b>#{message}!"
+  def send_email(to, subject, message, from = 'jordan@pigeonpic.com')
+    RestClient.post "https://api:#{ENV['MAILGUN_KEY']}@api.mailgun.net/v2/pigeonpic.com"+"/messages", 
+    :from => from,
+    :to => to,
+    :subject => subject,
+    :html => message
+  end
+
+  private
+  def basic_template(params)
+    p params
+    html = ""
+    html += "<p><b>Hello #{params[:name]},</b><br><br>"
+    params[:paragraphs].each do |paragraph|
+      html += "#{paragraph}<br><br>"
+    end
+    html += "<p style=\"font-size:16px;\">Jordan Nemrow<br>
+    <span style=\"font-size:14px;\">Pigeon</span><br>
+    <span style=\"font-size:12px; font-style:italic\">gettin physical with photos again</span>
+    </p>"
+    html
+  end
+
+  def quote_escape(content)
+    content.gsub(/'/, "\\'").gsub(/"/,'\\"')
   end
 end
